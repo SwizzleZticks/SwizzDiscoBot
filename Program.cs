9b55ea14b741c8147fa081f86ec6d5276b5ea7ac
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2024-12-20 16:13:00 -0500

    update to .net 8.0

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using SwizzBotDisco.Services;
+
+namespace SwizzBotDisco
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            await DiscordBotService.StartServiceAsync();
+        }
+    }
+}
---
+++ Services/DiscordBotService.cs
@@ -0,0 +1,30 @@
+﻿using Discord.WebSocket;
+using Discord;
+using SwizzBotDisco.Config;
+
+namespace SwizzBotDisco.Services
+{
+    public class DiscordBotService
+    {
+        private static DiscordSocketClient _client;
+
+        public static async Task StartServiceAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            string token = await JSONReader.GetTokenAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private static Task Log(LogMessage msg) //Create logging service later and move
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
