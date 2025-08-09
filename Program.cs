6dcfbc2dafab42a052497728b050055f93b99c48
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2025-08-08 23:22:56 -0400

    add logging

--- Program.cs
+++ Program.cs
@@ -18,6 +18,26 @@ namespace SwizzBotDisco
 
             var service = new InteractionService(client);
 
+            client.Log += msg =>
+            {
+                Console.WriteLine($"[Client] {msg}");
+                return Task.CompletedTask;
+            };
+
+            service.Log += msg =>
+            {
+                Console.WriteLine($"[InteractionService] {msg}");
+                return Task.CompletedTask;
+            };
+
+            service.SlashCommandExecuted += async (cmd, ctx, result) =>
+            {
+                if (!result.IsSuccess)
+                    Console.WriteLine($"[SlashCommand Error] {result.Error} | {result.ErrorReason}");
+            };
+
+
+
             await service.AddModulesAsync(Assembly.GetExecutingAssembly(), services: null);
 
             client.InteractionCreated += async interaction =>
