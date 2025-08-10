36a676b71bfc3b8ffb86212f8ebac48e48f5c465
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2025-08-09 20:41:48 -0400

    add interaction service

--- Services/DiscordBotService.cs
+++ Services/DiscordBotService.cs
@@ -1,5 +1,7 @@
-﻿using Discord.WebSocket;
-using Discord;
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System.Reflection;
 
 namespace SwizzBotDisco.Services
 {
@@ -10,15 +12,26 @@ namespace SwizzBotDisco.Services
         public static async Task StartServiceAsync()
         {
             string? token = Environment.GetEnvironmentVariable("BOT_TOKEN");
-            
+
             _client = new DiscordSocketClient();
+            var interactionService = new InteractionService(_client);
+
             _client.Log += Log;
-            
+
+
+            _client.InteractionCreated += async interaction =>
+            {
+                var ctx = new SocketInteractionContext(_client, interaction);
+                await interactionService.ExecuteCommandAsync(ctx, null);
+            };
+
+            await interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), null);
 
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
         }
-        
+
+
         public static DiscordSocketClient Client => _client;
 
         private static Task Log(LogMessage msg) //Create logging service later and move
